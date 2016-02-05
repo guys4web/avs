7cab70276d68f72b1f594f13532f9cb0f8a098dc
Ahmed Shams <zangee3@gmail.com>
2016-02-05 00:51:47 -0500

    clear order when service is changed

--- public/assets/js/pages/cart.js
+++ public/assets/js/pages/cart.js
@@ -1,9 +1,20 @@
 $(function () {   
 
 	var fee = 0;
+	var order = {
+    	clear: function() {
+    		$('#visa_type').text('');
+	    	$('#visa_fee').text('');
+	    	$('#quantity').text('');
+	    	$('#order_total').text('');
+	    	$('.visas').prop('checked', false);
+    	}
+    }
+
 	$('#services').change(function() {        
         //console.log(this.value, $(this).find("option:selected").text());
         $('#service_type').text($(this).find("option:selected").text());
+        order.clear();
     });
 
     $('.visas').click(function() {
@@ -18,6 +29,7 @@ $(function () {
     $('#qty').change(function() {        
        console.log(fee + '--fee');
         $('#order_total').text($(this).val() * fee);
-    });
+    });    
+    
 
 });
