bc3a05c189167dcf2ef4383ae6ba5466a3b5a3c9
unknown <lala.misa.09@googlemail.com>
2016-02-11 15:56:24 +0300

    track order - add passengers list popup

--- resources/views/partials/passengers.blade.php
+++ resources/views/partials/passengers.blade.php
@@ -1,48 +1,68 @@
+@foreach($cart->cartItems as $item)
 <div class="row">
   <div class="col-xs-12">
+    <h3>
+      {{  $item->product->service->name }}
+      <br/>
+      {{ $item->product->visa->name }}
+    </h3>
       <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+          @foreach($item->passengers as $passenger)
           <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingOne">
+            <div class="panel-heading" role="tab" id="heading{{ $passenger->id  }}">
               <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  Collapsible Group Item #1
+                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $passenger->id  }}" aria-expanded="true" aria-controls="collapse{{ $passenger->id  }}">
+                  #{{ $passenger->id  }}
                 </a>
               </h4>
             </div>
-            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+            <div id="collapse{{ $passenger->id  }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading{{ $passenger->id  }}">
               <div class="panel-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  <table class="table table-striped">
+                      <tr>
+                          <td>
+                              <small class="text-uppercase gra" >Gender</small>
+                              <br/>
+                              {{  $passenger->gender }}
+                          </td>
+                          <td>
+                              <small class="text-uppercase gra">Birthday</small>
+                              <br/>
+                              {{  $passenger->birthday }}
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>
+                              <small class="text-uppercase gra">First name</small>
+                              <br/>
+                              {{  $passenger->first_name }}
+                          </td>
+                          <td>
+                              <small class="text-uppercase gra">Last name</small>
+                              <br/>
+                              {{  $passenger->last_name }}
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>
+                              <small class="text-uppercase gra">PassPort Num</small>
+                              <br/>
+                              {{  $passenger->passport_num }}
+                          </td>
+                          <td>
+                              <small class="text-uppercase gra">PassPort ExpDate</small>
+                              <br/>
+                              {{  $passenger->passport_expirate }}
+                          </td>
+                      </tr>
+                  </table>
               </div>
             </div>
           </div>
-          <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingTwo">
-              <h4 class="panel-title">
-                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  Collapsible Group Item #2
-                </a>
-              </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-              <div class="panel-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
-          <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingThree">
-              <h4 class="panel-title">
-                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  Collapsible Group Item #3
-                </a>
-              </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-              <div class="panel-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-              </div>
-            </div>
-          </div>
+          @endforeach
+          <!-- end -->
+          <!-- end -->
         </div>
     </div>
 </div>
+@endforeach
